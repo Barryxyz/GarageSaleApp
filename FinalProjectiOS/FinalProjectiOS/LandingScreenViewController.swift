18d42f8d68b20e4112135b373076e946e2e83af9
Tin <tin@Christophers-MacBook-Pro.local>
2018-11-13 23:00:10 -0500

    Added wirefram sketch into xcode project

---
+++ FinalProjectiOS/FinalProjectiOS/LandingScreenViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LandingScreenViewController.swift
+//  FinalProjectiOS
+//
+//  Created by Tin on 11/13/18.
+//  Copyright © 2018 Tin. All rights reserved.
+//
+
+import UIKit
+
+class LandingScreenViewController: UIViewController {
+
+    @IBOutlet weak var UsernameTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func LoginButtonAction(_ sender: Any) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
