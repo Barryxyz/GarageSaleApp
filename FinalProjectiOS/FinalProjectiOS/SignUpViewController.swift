35d729839e8638ff9c2d999fab583ebdd1cb8825
Tin <tin@Christophers-MacBook-Pro.local>
2018-11-14 23:29:58 -0500

    Added picker view to signup form for contactmethod

--- FinalProjectiOS/FinalProjectiOS/SignUpViewController.swift
+++ FinalProjectiOS/FinalProjectiOS/SignUpViewController.swift
@@ -8,16 +8,42 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let contactPickerData = [String](arrayLiteral: "Phone Call", "Email", "Text Message")
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return contactPickerData.count
+    }
+    
+    func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return contactPickerData[row]
+    }
+    
+    func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        ContactMethod.text = contactPickerData[row]
+    }
+    
 
     @IBOutlet weak var firstName: UITextField!
     @IBOutlet weak var lastName: UITextField!
-    @IBOutlet var email: UIView!
+    @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
-    @IBOutlet var cPassword: UIView!
-    @IBOutlet var phoneNumber: UIView!
+    @IBOutlet weak var cPassword: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var ContactMethod: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let contactPicker = UIPickerView()
+        ContactMethod.inputView = contactPicker
+        contactPicker.delegate=self
+
         
         // Do any additional setup after loading the view.
     }
