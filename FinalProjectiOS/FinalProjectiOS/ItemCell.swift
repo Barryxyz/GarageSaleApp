654cace178baae1c59a9281c9444eef35e5283b1
Tin <tin@d-172-25-193-33.dhcp.virginia.edu>
2018-11-30 17:52:59 -0500

    added some initial table view stuff

---
+++ FinalProjectiOS/FinalProjectiOS/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  FinalProjectiOS
+//
+//  Created by Tin on 11/30/18.
+//  Copyright © 2018 Tin. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
---
+++ FinalProjectiOS/FinalProjectiOS/ItemListViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ItemListViewController.swift
+//  FinalProjectiOS
+//
+//  Created by Tin on 11/30/18.
+//  Copyright © 2018 Tin. All rights reserved.
+//
+
+import UIKit
+//var saleItemsList = [saleItem]()
+// add this later UITableViewDataSource, UITableViewDelegate
+
+class ItemListViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//    }
+//
+    
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return bucketList.count
+//    }
+//
+//    // Override to show what each cell should have in it based on the note in the list
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//        // Table view cells are reused and should be dequeued using a cell identifier.
+//        let cellIdentifier = "BucketListTableViewCell"
+//
+//        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! BucketListTableViewCell
+//
+//        // Fetches the appropriate note for the data source layout.
+//        let item = bucketList[indexPath.row]
+//
+//        cell.nameLabel.text = item.name
+//
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateStyle = .short
+//        let convertedDate = dateFormatter.string(from: item.date)
+//        cell.dateLabel.text = convertedDate
+//
+//        //set color to green if it is completed
+//        if (item.doneStatus == true){
+//            cell.backgroundColor = UIColor.green
+//        }
+//            //otheriwse, item color should be white (default color)
+//        else{
+//            cell.backgroundColor = UIColor.white
+//        }
+//        return cell
+//    }
+//
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
